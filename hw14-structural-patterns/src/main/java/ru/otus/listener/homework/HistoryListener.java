06bde2a512cf8268806504ac564d93fc6ebc2928
VladislavBodikov <krullerr@yandex.ru>
2023-10-19 22:05:06 +0300

    hw14 patterns - listener

--- hw14-structural-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw14-structural-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,17 +2,32 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class HistoryListener implements Listener, HistoryReader {
 
+    Map<Long, Message> messageHistory = new ConcurrentHashMap<>();
+
+
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        messageHistory.put(
+                msg.getId(),
+                msg.toBuilder().field13(ObjectForMessage.from(msg.getField13())).build()
+        );
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        if (messageHistory.containsKey(id)){
+            return Optional.of(messageHistory.get(id).toBuilder().build());
+        }
+        return Optional.empty();
     }
 }
